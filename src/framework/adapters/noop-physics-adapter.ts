b1279d52348b9352cb805a6525098216fee9dd14
Den4iK <gerasymenkoden@gmail.com>
2026-04-25 11:01:00 +0200

    добавил 2 варианта

--- src/framework/adapters/noop-physics-adapter.ts
+++
@@ -1,7 +0,0 @@
-import type { PhysicsAdapter, PhysicsContact } from './physics';
-
-export class NoopPhysicsAdapter implements PhysicsAdapter {
-  step(_deltaTime: number): readonly PhysicsContact[] {
-    return [];
-  }
-}
